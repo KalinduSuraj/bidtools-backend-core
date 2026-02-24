8d9909c6795a169ca5b6b83ccb9d7882a1e72931
KalinduSuraj <mwkalindusuraj@gmail.com>
2026-02-24 21:19:48 +0530

    feat: add DynomodbModule import to RentalModule for enhanced functionality

--- src/rental/rental.module.ts
+++ src/rental/rental.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { RentalService } from './rental.service';
 import { RentalController } from './rental.controller';
 import { RentalRepository } from './rental.repository';
+import { DynomodbModule } from '../common/dynomodb/dynomodb.module';
 
 @Module({
+  imports: [DynomodbModule],
   controllers: [RentalController],
   providers: [RentalService, RentalRepository],
 })
